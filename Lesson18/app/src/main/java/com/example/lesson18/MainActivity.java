b9151b23a847339df223966664de23e792cc972e
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-09-22 22:32:27 +0600

    upload

--- Lesson18/app/src/main/java/com/example/lesson18/MainActivity.java
+++ Lesson18/app/src/main/java/com/example/lesson18/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.lesson18;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View
+        spinner=findViewById(R.id.spinner);
+
+        //Data Source:
+        String[] courses= {"C++","Java","Kotlin","Python"};
+
+        // ArrayAdapter: used to populate the 'Spinner' with
+        //               items from a string array resource
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
