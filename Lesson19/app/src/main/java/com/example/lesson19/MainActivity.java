810dc4fe1c33a37f64bc5afeba2cbd4356e00846
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-09-22 22:59:11 +0600

    Lesson20

--- Lesson19/app/src/main/java/com/example/lesson19/MainActivity.java
+++ Lesson19/app/src/main/java/com/example/lesson19/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lesson19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TimePicker timePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker=findViewById(R.id.timePicker);
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int i, int i1) {
+                // Handle Time Change
+                Toast.makeText(MainActivity.this, "Hour: "+i+" minute: "+i1, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
