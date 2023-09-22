03968748fe44d18a2a946cf7e035583a6a8077ef
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-09-22 23:32:34 +0600

    Lesson20

--- Lesson20/app/src/main/java/com/example/lesson20/MainActivity.java
+++ Lesson20/app/src/main/java/com/example/lesson20/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.lesson20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatePicker datePicker;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        datePicker=findViewById(R.id.datePicker);
+        btn=findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String day = "Day "+ datePicker.getDayOfMonth();
+                String month = "Month "+ (datePicker.getMonth()+1);
+                String year = "Year "+ datePicker.getYear();
+
+                Toast.makeText(MainActivity.this,
+                        day+" "+month+" "+year,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
