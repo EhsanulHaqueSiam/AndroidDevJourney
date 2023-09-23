2157f53391aa291476361fe036d8c83c1b62fb4f
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-09-23 18:38:12 +0600

    Lesson23

---
+++ Lesson23_AdaptersApp/app/src/main/java/com/example/lesson23_adaptersapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.lesson23_adaptersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 1- AdapterView: ListView
+        listView=findViewById(R.id.listview);
+
+        // 2- Data Source: String Array
+        String[] countries={"USA","Germany","Saudi Arab","France"};
+
+
+        // 3- Adapter: acts as a bridge between the
+        //          'data Source' and the 'AdapterView'
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+//                this,
+//                android.R.layout.simple_list_item_1,
+//                countries
+//        );
+        //Custom Adapter
+        MyCustomAdapter myCustomAdapter=new MyCustomAdapter(this,countries);
+
+        // Link Listview with the Adapter
+        listView.setAdapter(myCustomAdapter);
+    }
+}
---
+++ Lesson23_AdaptersApp/app/src/main/java/com/example/lesson23_adaptersapp/MyCustomAdapter.java
@@ -0,0 +1,56 @@
+package com.example.lesson23_adaptersapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class MyCustomAdapter extends BaseAdapter {
+
+    private Context context;
+    private String[] items; //Data Source
+
+    public MyCustomAdapter(Context context, String[] items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.length; // Returns the number of items in your data Source
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items[i]; // Returns the data item at the given position
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+
+        if (view == null) {
+            // convertView/View: is a recycled View that you can reuse to
+            //        improve the performance of your list.
+            view= LayoutInflater.from(context).inflate(R.layout.my_list_item,viewGroup,false);
+        }else {
+            // Reusing the View (that's recycled)
+            holder= (ViewHolder) view.getTag();
+        }
+
+        return null; // Displays the data at a position in the data set
+    }
+
+    static class ViewHolder{
+        // Holds references to the views within an item layout
+
+        TextView textView;
+    }
+}
