285675d2850858e19e8f4bebc03f6b520012ce45
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-09-25 23:08:51 +0600

    Lesson28 (AndroidPdfViewer)

---
+++ Lesson28/app/src/main/java/com/example/lesson28/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lesson28;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PDFView pdfView=findViewById(R.id.pdfView);
+
+        pdfView.fromAsset("dummy.pdf")
+                .load();
+    }
+}
