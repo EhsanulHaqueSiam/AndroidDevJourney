cd68b73c268886ece85361dd3d5d5057fee07273
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2024-01-14 04:33:55 +0600

    Lesson49 MovieProApp

---
+++ Lesson49_MovieProApp/app/src/main/java/com/example/lesson49_movieproapp/model/Movie.java
@@ -0,0 +1,8 @@
+package com.example.lesson49_movieproapp.model;
+
+import android.os.Parcelable;
+
+import androidx.databinding.BaseObservable;
+
+public class Movie extends BaseObservable implements Parcelable {
+}
