b913f2e2b0459f6496630342fd91a3f3a0529ca1
Md Ehsanul Haque <ehsanul.siamdev@gmail.com>
2024-01-28 01:05:35 +0600

    Movie Pro app Used Glide , Retrofit, Paging, RxJava3 Notations, Hilt Dagger, LifeCycle - ViewModel LiveData

---
+++ Lesson50_PagingApp/app/src/main/java/com/example/lesson50_pagingapp/MovieApp.java
@@ -0,0 +1,9 @@
+package com.example.lesson50_pagingapp;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class MovieApp extends Application {
+}
